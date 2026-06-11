add63c33e1ad0999994d30a3b61d4a0c885367a7
Maria <dobrydnyk.m@gmail.com>
2026-06-11 13:28:41 +0300

    Create Header function

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
-
+import { Button } from "./components/Button";
 
 export default function App() {
-  return null
+  return (
+    <div className="max-w-2xl mx-auto p-4 flex-col gap-4" >
+      <Header></Header>
+    </div>
+  )
 }
 
+function Header() {
+  return (
+    <header className="flex items-center justify-between">
+    <div className="flex flex-col gap-1">
+      <h1 className="text-3xl font-bold ">Habit Tracker</h1>
+      <span className="text-zinc-400 text-sm">1 / 1 done today</span>
+    </div>
+    
+    <div className="flex flex-col gap-1 items-end">
+      <span className="text-zinc-400 text-sm">Jun 8 - Jun 14</span>
+      <div className="flex items-center gap-3"> 
+        <Button>Prev</Button>
+        <Button>Next</Button>
+      </div>
+    </div>
+  </header>
+  )
+}

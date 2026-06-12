7e53b5df38258c216a233e24b2cb66b487574bce
Maria <dobrydnyk.m@gmail.com>
2026-06-12 18:43:24 +0300

    Create separate file for Habit Form

---
+++ src/components/HabitForm.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./Button";
+
+export function HabitForm() {
+    return (
+      <form action="" className="flex gap-2">
+        <input className="flex-1 gap-1 bg-zinc-800 px-4 py-2 rounded-lg outline-none focus-visible:ring-2 focus-visible:ring-violet-500" type="text" placeholder="New habit..."/>
+        <Button>Add Habit</Button>
+      </form>
+    )
+  };

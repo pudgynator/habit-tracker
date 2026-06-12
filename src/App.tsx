c3bd3e50e2fc29d3bab81a9d5f994bfe85bcd83e
Maria <dobrydnyk.m@gmail.com>
2026-06-12 19:19:15 +0300

    Add HeaderList function

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Header } from "./components/Header";
 import { HabitForm } from "./components/HabitForm";
+import { HeaderList } from "./components/HeaderList";
 
 export default function App() {
   return (
     <div className="max-w-2xl mx-auto p-4 flex-col gap-4" >
       <Header />
       <HabitForm />
+      <HeaderList />
     </div>
   )
 }

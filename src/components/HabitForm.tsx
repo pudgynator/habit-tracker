506ace957db27e0a49c0288f37629cc3662d4c8b
Maria <dobrydnyk.m@gmail.com>
2026-06-12 19:21:06 +0300

    Change styles

--- src/components/HabitForm.tsx
+++ src/components/HabitForm.tsx
@@ -2,7 +2,7 @@ import { Button } from "./Button";
 
 export function HabitForm() {
     return (
-      <form action="" className="flex gap-2">
+      <form action="" className="flex gap-2 mb-4">
         <input className="flex-1 gap-1 bg-zinc-800 px-4 py-2 rounded-lg outline-none focus-visible:ring-2 focus-visible:ring-violet-500" type="text" placeholder="New habit..."/>
         <Button>Add Habit</Button>
       </form>

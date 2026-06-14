ea87596f1351ac6b1efd183229264702e4693a9b
Maria <dobrydnyk.m@gmail.com>
2026-06-14 17:19:52 +0300

    add habit creation functionality"

--- src/components/HabitForm.tsx
+++ src/components/HabitForm.tsx
@@ -1,12 +1,38 @@
+import { useState, type SubmitEvent } from "react";
 import { Button } from "./Button";
 
-export function HabitForm() {
-    return (
-      <form action="" className="flex gap-2 mb-4">
-        <input className="flex-1 gap-1 bg-zinc-800 px-4 py-2 rounded-lg 
-        outline-none focus-visible:ring-2 focus-visible:ring-violet-500" type="text" placeholder="New habit..."/>
+type HabitFormProps = {
+  addHabit: (name: string) => void;
+}
 
-        <Button className="rounded-lg px-4 py-2 font-medium">Add Habit</Button>
+export function HabitForm({ addHabit }: HabitFormProps) {
+    const [name, setName] = useState('');
+
+    function handleSubmit(e: SubmitEvent) {
+        e.preventDefault()
+
+        if(name.trim() === '') {
+          return
+        }
+
+        setName('')
+        addHabit(name)
+    }
+
+    return (
+      <form action="" className="flex gap-2 mb-4" onSubmit={handleSubmit}>
+        <input
+          value={name}
+          onChange={e => setName(e.target.value)}
+          className="flex-1 gap-1 bg-zinc-800 px-4 py-2 rounded-lg outline-none focus-visible:ring-2 focus-visible:ring-violet-500" 
+          type="text" 
+          placeholder="New habit..."/>
+
+        <Button 
+          disabled={name.trim() === ''} 
+          className="rounded-lg px-4 py-2 font-medium">
+            Add Habit
+        </Button>
       </form>
     )
   };

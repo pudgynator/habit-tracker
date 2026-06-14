949a7ac5c30b4620750bcfa523fb55eda7180022
Maria <dobrydnyk.m@gmail.com>
2026-06-14 17:18:25 +0300

    Add new functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,40 @@
 import { Header } from "./components/Header";
 import { HabitForm } from "./components/HabitForm";
-import { HeaderList } from "./components/HabitList";
+import { HabitList, type Habit } from "./components/HabitList";
+import { useState } from "react";
+import { isSameDay } from "date-fns";
 
 export default function App() {
+  const [habits, setHabits] = useState<Habit[]>([])
+
+  function addHabit(name: string) {
+    setHabits(curr => [...curr, {id: crypto.randomUUID(), name, completions: [] }] )
+  }
+
+  function deleteHabit(id: string) {
+    setHabits(curr => curr.filter(h => h.id !== id))
+  }
+
+  function toggleHabit(id: string, date: Date) {
+    setHabits(curr =>  
+      curr.map(h => {
+        if(h.id !== id) return h
+        
+        const alreadyDone = h.completions.some(c => isSameDay(c, date))
+        const completions = alreadyDone 
+          ? h.completions.filter(c => !isSameDay(c, date))
+          : [...h.completions, date]
+
+        return { ...h, completions }
+      }),
+    )
+  }
+
   return (
     <div className="max-w-2xl mx-auto p-4 flex-col gap-4" >
       <Header />
-      <HabitForm />
-      <HeaderList />
+      <HabitForm addHabit={addHabit}/>
+      <HabitList deleteHabit={deleteHabit} toggleHabit={toggleHabit} habits={habits}/>
     </div>
   )
 }

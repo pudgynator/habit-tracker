1422f9557ff4f5184a28f28d409dc1be5a28df94
Maria <dobrydnyk.m@gmail.com>
2026-06-19 14:14:36 +0300

    Remove ; sign

--- src/components/Contribution/MonthLables.tsx
+++ src/components/Contribution/MonthLables.tsx
@@ -41,7 +41,7 @@ export function MonthLabels({ monthName, monthColumnMap}: MonthProps) {
                         </div>
                     );
                 })
-            };      
+            }
         </div>
 
     )

b7f58a6743fe59b482c809f4e65915059ad94407
Maria <dobrydnyk.m@gmail.com>
2026-06-19 14:14:03 +0300

    Add day labels

--- src/components/Contribution/Contribution.tsx
+++ src/components/Contribution/Contribution.tsx
@@ -14,23 +14,33 @@ export function Contribution({  yearDates, monthName }: ContributionProps) {
     const { habits } = useHabits();
 
     const monthColumnMap = useMemo(() => 
-        createMonthColumnMap(yearDates), [yearDates]
-    )
+        createMonthColumnMap(yearDates), [yearDates])
 
     return (
-
         <div className="bg-zinc-800 px-4 py-2  rounded-lg ">
 
-            <div className="overflow-x-auto">
-                <MonthLabels
-                    monthName={monthName}
-                    monthColumnMap={monthColumnMap}
-                />
+            <div className="flex">
+                <div className="grid grid-rows-7 gap-[3px] pr-2 mt-[28px] h-[105px] text-[10px] select-none shrink-0">
+                    <div></div>
+                    <div className="flex items-center font-medium">Mon</div> 
+                    <div></div>
+                    <div className="flex items-center font-medium">Wed</div> 
+                    <div></div>
+                    <div className="flex items-center font-medium">Fri</div> 
+                    <div></div>
+                </div>
 
-                <ContributionGrid 
-                    yearDates={yearDates}
-                    habits={habits}
-                />
+                <div className="overflow-x-auto flex-1">
+                    <MonthLabels
+                        monthName={monthName}
+                        monthColumnMap={monthColumnMap}
+                    />
+
+                    <ContributionGrid 
+                        yearDates={yearDates}
+                        habits={habits}
+                    />
+                </div>
             </div>
 
             <div className="flex sticky  w-max right-0 ml-auto gap-0.5 items-center">

894be548e5f280388a1f405b4afd11931889e8b3
Maria <dobrydnyk.m@gmail.com>
2026-06-19 13:11:27 +0300

    change month interval

--- src/App.tsx
+++ src/App.tsx
@@ -5,50 +5,53 @@ import { HabitProvider } from "./context/HabitProvider";
 import { Contribution } from "./components/Contribution";
 import { useState } from "react";
 import {
-  addWeeks,
-  eachDayOfInterval,
-  eachMonthOfInterval,
-  endOfWeek,
-  endOfYear,
-  startOfWeek,
-  startOfYear,
-  subYears,
+    addWeeks,
+    eachDayOfInterval,
+    eachMonthOfInterval,
+    endOfMonth,
+    endOfWeek,
+    startOfMonth,
+    startOfWeek,
+    subYears,
 } from "date-fns";
 
 
 export default function App() {
-  const [weekOffset, setWeekOffset] = useState(0);
+    const [weekOffset, setWeekOffset] = useState(0);
 
-  const date = new Date();
+    const date = new Date();
+    //date.setDate(date.getDate());
+    const week = addWeeks(date, weekOffset);
+    const visibleDates = eachDayOfInterval({
+        start: startOfWeek(week, { weekStartsOn: 0 }),
+        end: endOfWeek(week, { weekStartsOn: 0 }),
+    });
 
-  const week = addWeeks(date, weekOffset);
-  const visibleDates = eachDayOfInterval({
-    start: startOfWeek(week, { weekStartsOn: 0 }),
-    end: endOfWeek(week, { weekStartsOn: 0 }),
-  });
+    const yearDates = eachDayOfInterval({
+        start: startOfWeek(subYears(date, 1), { weekStartsOn: 0 }),
+        end: endOfWeek(date, { weekStartsOn: 0 }),
+    });
 
-  const monthName = eachMonthOfInterval({
-    start: startOfYear(date),
-    end: endOfYear(date),
-  });
+    const monthName = eachMonthOfInterval({
+        start: startOfMonth(yearDates[0]),
+        end: endOfMonth(yearDates[yearDates.length - 1]),
+    });
 
-  const yearDates = eachDayOfInterval({
-    start: startOfWeek(subYears(date, 1), { weekStartsOn: 0 }),
-    end: endOfWeek(date, { weekStartsOn: 0 }),
-  });
-
-  return (
-    <div className="max-w-2xl mx-auto p-4 flex-col gap-4">
-      <HabitProvider>
-        <Header
-          visibleDates={visibleDates}
-          onPrev={() => setWeekOffset((O) => O - 1)}
-          onNext={() => setWeekOffset((O) => O + 1)}
-        />
-        <HabitForm />
-        <HabitList visibleDates={visibleDates} />
-        <Contribution yearDates={yearDates} monthName={monthName} />
-      </HabitProvider>
-    </div>
-  );
+    return (
+        <div className="max-w-2xl mx-auto p-4 flex-col gap-4">
+        <HabitProvider>
+            <Header
+                visibleDates={visibleDates}
+                onPrev={() => setWeekOffset((O) => O - 1)}
+                onNext={() => setWeekOffset((O) => O + 1)}
+            />
+            <HabitForm />
+            <HabitList visibleDates={visibleDates} />
+            <Contribution 
+                yearDates={yearDates} 
+                monthName={monthName} 
+            />
+        </HabitProvider>
+        </div>
+    );
 }
